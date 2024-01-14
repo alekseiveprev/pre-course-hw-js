87bc941536702560a0ca7832ac5edb4ed3bdecc8
alekseiveprev <alekseiveprev@gmail.com>
2024-01-14 21:41:06 +0300

    test 5-2

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -13,7 +13,8 @@ default:...
 } */
 
 //hw5
-let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+//TEST1
+/* let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
 switch (true) {
     case rule.length < 25:
@@ -24,4 +25,21 @@ switch (true) {
         break; 
 
     default:console.log("50 на 50");
+} */
+
+//TEST2
+let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+
+switch (true) {
+    case rule.length < 25:
+        console.log("Все таки нет правил без исключения");
+        break; 
+    case rule.length > 25:
+        console.log("У меня 100 пудов все получится");
+        break; 
+    case rule.length == 25:
+        console.log("50 на 50");
+        break; 
+
+   // default:console.log("50 на 50");
 }
