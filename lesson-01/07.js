9bdcc4b3a05804e77189c511d666da77fcf01f73
alekseiveprev <alekseiveprev@gmail.com>
2024-01-14 22:09:18 +0300

    test 7-1

---
+++ lesson-01/07.js
@@ -0,0 +1,17 @@
+/* 7 задание
+
+Дано:
+
+​let iAm = 'я'
+let krasavchik = "красавчик!";
+📋
+  Выведи в консоль «Я красавчик!».
+❗ Обрати внимание: «Я» с заглавной. */
+
+//hw7
+
+let iAm = 'я'
+let krasavchik = "красавчик!";
+console.log( iAm.toUpperCase() +' '+ krasavchik );
+
+
