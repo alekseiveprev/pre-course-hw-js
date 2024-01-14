91531973c3fd527aca5a5a00896da5c80d76508d
alekseiveprev <alekseiveprev@gmail.com>
2024-01-14 21:21:05 +0300

    test 3-1

---
+++ lesson-01/03.js
@@ -0,0 +1,21 @@
+/* 3 задание
+Имея в распоряжении только:
+​let number = 1;
+и используя лишь
+--number   number--  ++number   number++
+Получи в консоли 1,3,3,1 
+❗Нужно вызвать консоль ЧЕТЫРЕ раза
+Пример:
+
+console.log(number**); 
+console.log(**number); 
+console.log(number**); 
+console.log(**number);  */
+
+//hw3
+let number = 1;
+
+console.log(number++); //1
+console.log(++number); //3
+console.log(number--); //3
+console.log(--number); //1
